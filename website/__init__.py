82b20c58006e4fe69b3d721fba0a067cd99a88c7
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-11 15:15:44 +0100

    Add Flask app factory function to create_app() in website/__init__.py

---
+++ website/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from os import path
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'mqkjfmkrzokznk16545612151djbsjdj@!!?105ee5d2d2d5f'
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+    return app
